573c17f1153702976f0f63cbe34bb2cc97af4c9b
José Miguel Horcas <jhorcas@us.es>
2021-01-26 16:25:27 +0100

    Basic and implementation independent strategy for MonteCarlo

---
+++ montecarlo4fms/algorithms/montecarlo_basic.py
@@ -0,0 +1,35 @@
+from montecarlo4fms.algorithms import MonteCarlo
+
+class MonteCarloBasic(MonteCarlo):
+    """
+    Basic implementation of the MonteCarlo strategy.
+    It first runs n simulations, then chooses the best state.
+    """
+
+    def __init__(self, n_simulations: int):
+        self.n_simulations = n_simulations
+
+    def choose(self, state: State) -> State:
+        if state.is_terminal():
+            raise RuntimeError(f"Montecarlo choose method called on terminal state {state}")
+
+        self.Q = defaultdict(int)   # total reward of each state
+        self.N = defaultdict(int)   # total visit count of each state
+        successors = state.find_successors()
+        for i in range(self.n_simulations):
+            s = random.choice(successors)
+            reward = self.simulate(s)
+            self.Q[s] += s.reward()
+            self.N[s] += 1
+
+        return max(self.Q.keys(), key=self.score)
+
+    def simulate(self, state: State) -> int:
+        while not state.is_terminal():
+            state = state.find_random_successor()
+        return state.reward()
+
+    def score(self, state: State) -> float:
+        if self.N[state] == 0:
+            return float("-inf")              # avoid unseen state
+        return self.Q[state] / self.N[state]  # average reward

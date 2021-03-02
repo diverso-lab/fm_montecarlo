1322cbf6d6ecf204ee00ae76ab8687d8d05daf47
José Miguel Horcas <jhorcas@us.es>
2021-03-02 14:39:48 +0100

    Add new feature models for evaluation

--- tests/test_featureIDE_parser.py
+++ tests/test_featureIDE_parser.py
@@ -1,5 +1,8 @@
-from famapy.metamodels.fm_metamodel.transformations import FeatureIDEParser
 import unittest
+
+from famapy.metamodels.fm_metamodel.transformations import FeatureIDEParser
+
+from famapy.metamodels.fm_metamodel.utils import fm_utils
 
 
 class TestFeatureIDEParser(unittest.TestCase):
@@ -8,9 +11,12 @@ class TestFeatureIDEParser(unittest.TestCase):
         self.parser = FeatureIDEParser
         self.ext = '.' + self.parser.get_source_extension()
         self.input_folder = '../input_fms/'
-        self.models = {'linux-2.6.33.3basic': [44079, 28821, 'root', 2238],      # fm_name -> nof_features, nof_constraints, root_name, core_features
-                       'automotive2_1basic':  [14098,   833, 'N_379925076__F_91527E35945B', 1412],
-                       'pizzas':              [12, 1, 'Pizza', 4]}
+        self.models = {'wget':                [17, 0, 'wget', 2, 0, 1]          # fm_name -> nof_features, nof_constraints, root_name, core_features, or-groups, alternative-groups
+
+                      }
+                       # 'linux-2.6.33.3basic': [44079, 28821, 'root', 2238],
+                       # 'automotive2_1basic':  [14098,   833, 'N_379925076__F_91527E35945B', 1412],
+                       # 'pizzas':              [12, 1, 'Pizza', 4]}
 
     def test_nof_features(self):
         for fm_input in self.models:
@@ -33,6 +39,22 @@ class TestFeatureIDEParser(unittest.TestCase):
             with self.subTest(fm=fm_input):
                 self.assertEqual(fm.root.name, self.models[fm_input][2])
                 self.assertIsNone(fm.root.get_parent())
+
+    def test_or_groups(self):
+        for fm_input in self.models:
+            parser = self.parser(self.input_folder + fm_input + self.ext)
+            fm = parser.transform()
+            with self.subTest(fm=fm_input):
+                or_groups = [f for f in fm.get_features() if fm_utils.is_or_group(f)]
+                self.assertEqual(len(or_groups), self.models[fm_input][4])
+
+    def test_alternative_groups(self):
+        for fm_input in self.models:
+            parser = self.parser(self.input_folder + fm_input + self.ext)
+            fm = parser.transform()
+            with self.subTest(fm=fm_input):
+                alternative_groups = [f for f in fm.get_features() if fm_utils.is_alternative_group(f)]
+                self.assertEqual(len(alternative_groups), self.models[fm_input][5])
 
 
 if __name__ == "__main__":
